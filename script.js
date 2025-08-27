af6bff4f6524df963ac6ab63bdf25fe8682764ec
santosyummi <141366425+santosyummi@users.noreply.github.com>
2025-08-27 12:36:08 +0000

    ok

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-.
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclik = diminuiTamanho;
+botoes[1].onclik = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha -1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+        tamanhoSenha = tamanhoSenha +1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+funcition diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha -1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+funcition aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha +1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+
+console.log(botoes)
